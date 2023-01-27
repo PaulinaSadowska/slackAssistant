cf3b73076677cc826ec98c9a3c6f50440e4b7a8a
Paulina Sadowska <PaulinaSadowska@users.noreply.github.com>
2023-01-27 17:20:04 +0100

    simplify analyze

--- src/samples/analyzeHistory.ts
+++ src/samples/analyzeHistory.ts
@@ -9,18 +9,8 @@ fs.readFile(config.filenames.history, (err, data) => {
     if (err) throw err;
     let threads: Thread[] = JSON.parse(data.toString());
 
-    console.log("DAILY ANALYSIS")
-    const analysisPerDay = countMessagesPerDay({ threads: threads, excludeBots: false, keywords: config.keywords })
-    const averagePerDay = averageThreadStats(analysisPerDay)
-    console.log(averagePerDay);
-
     console.log("MONTHLY ANALYSIS")
     const analysisPerMonth = countMessagesPerMonth({ threads: threads, excludeBots: true, keywords: config.keywords })
     const averagePerMonth = averageThreadStats(analysisPerMonth)
     console.log(averagePerMonth);
-
-    console.log("YEARLY ANALYSIS")
-    const analysisPerYear = countMessagesPerYear({ threads: threads, excludeBots: true, keywords: config.keywords })
-    const averagePerYear = averageThreadStats(analysisPerYear)
-    console.log(averagePerYear);
 });
