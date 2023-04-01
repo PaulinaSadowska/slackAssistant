3ecd7b5b44c35e20270cba7256b7f69e662adde4
Paulina Sadowska <PaulinaSadowska@users.noreply.github.com>
2023-04-01 22:08:18 +0200

    Change the way json is imported

--- src/utils/spreadsheet.ts
+++ src/utils/spreadsheet.ts
@@ -3,29 +3,40 @@ import { GoogleSpreadsheet } from 'google-spreadsheet';
 import config from '../config.js';
 
 export default function sendToSpreadsheet() {
-    const doc = new GoogleSpreadsheet(config.google.spreadsheetId);
-    const rows = [
-      ['John', 'Doe', 'john.doe@example.com'],
-      ['Jane', 'Smith', 'jane.smith@example.com'],
-      ['Bob', 'Jones', 'bob.jones@example.com'],
-    ];
+  const doc = new GoogleSpreadsheet(config.google.spreadsheetId);
+  const rows = [
+    ['John', 'Doe', 'john.doe@example.com'],
+    ['Jane', 'Smith', 'jane.smith@example.com'],
+    ['Bob', 'Jones', 'bob.jones@example.com'],
+  ];
 
-    const creds = require('../../google-credentials.json');
+  //const creds = require('.././google-credentials.json');
 
-    async function accessSpreadsheet() {
-      await doc.useServiceAccountAuth({
-        client_email: creds.client_email,
-        private_key: creds.private_key,
-      });
+  const modulePath = '.././google-credentials.json';
+
+  import(modulePath)
+    .then((creds) => {
       
-      await doc.loadInfo();
-    }
-    
-    async function writeToSpreadsheet() {
-      const sheet = doc.sheetsByIndex[0];
-      
-      await sheet.addRows(rows);
-    }
-    
-    accessSpreadsheet().then(writeToSpreadsheet);
+  async function accessSpreadsheet() {
+    await doc.useServiceAccountAuth({
+      client_email: creds.client_email,
+      private_key: creds.private_key,
+    });
+
+    await doc.loadInfo();
+  }
+
+  async function writeToSpreadsheet() {
+    const sheet = doc.sheetsByIndex[0];
+
+    await sheet.addRows(rows);
+  }
+
+  accessSpreadsheet().then(writeToSpreadsheet);
+    })
+    .catch((error) => {
+      console.error(`Failed to load module: ${modulePath}`, error);
+    });
+
+
 }
