030aafdd4d8e51fd089cf5fc1e1d09234f63f2b8
Paulina Sadowska <PaulinaSadowska@users.noreply.github.com>
2023-04-01 22:09:32 +0200

    last chance

--- src/utils/spreadsheet.ts
+++ src/utils/spreadsheet.ts
@@ -12,27 +12,27 @@ export default function sendToSpreadsheet() {
 
   //const creds = require('.././google-credentials.json');
 
-  const modulePath = '.././google-credentials.json';
+  const modulePath = '../google-credentials.json';
 
   import(modulePath)
     .then((creds) => {
-      
-  async function accessSpreadsheet() {
-    await doc.useServiceAccountAuth({
-      client_email: creds.client_email,
-      private_key: creds.private_key,
-    });
 
-    await doc.loadInfo();
-  }
+      async function accessSpreadsheet() {
+        await doc.useServiceAccountAuth({
+          client_email: creds.client_email,
+          private_key: creds.private_key,
+        });
 
-  async function writeToSpreadsheet() {
-    const sheet = doc.sheetsByIndex[0];
+        await doc.loadInfo();
+      }
 
-    await sheet.addRows(rows);
-  }
+      async function writeToSpreadsheet() {
+        const sheet = doc.sheetsByIndex[0];
 
-  accessSpreadsheet().then(writeToSpreadsheet);
+        await sheet.addRows(rows);
+      }
+
+      accessSpreadsheet().then(writeToSpreadsheet);
     })
     .catch((error) => {
       console.error(`Failed to load module: ${modulePath}`, error);
