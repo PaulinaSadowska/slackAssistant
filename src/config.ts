373ec8d14b07f08f105d6401739f673ef844becc
Paulina Sadowska <PaulinaSadowska@users.noreply.github.com>
2023-04-01 22:40:35 +0200

    test

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ const config = {
     },
     keywords: ["Android", "iOS", "backend", "Kotlin", "VTE", "PR", "wydanie"],
     google: {
-        //privateKey: process.env["GOOGLE_PRIVATE_KEY"]!,
+        privateKey: process.env["GOOGLE_PRIVATE_KEY"]!,
         spreadsheetId: process.env["GOOGLE_SPREADSHEET_ID"]!,
         email: process.env["GOOGLE_SERVICE_ACCOUNT_EMAIL"]!,
     }
