60d91fd132eb2baaf3648595dc01a38b3ab6e7ac
Paulina Sadowska <PaulinaSadowska@users.noreply.github.com>
2023-04-01 22:14:41 +0200

    other idea

--- src/utils/spreadsheet.ts
+++ src/utils/spreadsheet.ts
@@ -3,40 +3,30 @@ import { GoogleSpreadsheet } from 'google-spreadsheet';
 import config from '../config.js';
 
 export default function sendToSpreadsheet() {
-  const doc = new GoogleSpreadsheet(config.google.spreadsheetId);
-  const rows = [
-    ['John', 'Doe', 'john.doe@example.com'],
-    ['Jane', 'Smith', 'jane.smith@example.com'],
-    ['Bob', 'Jones', 'bob.jones@example.com'],
-  ];
+    const doc = new GoogleSpreadsheet(config.google.spreadsheetId);
+    const rows = [
+      ['John', 'Doe', 'john.doe@example.com'],
+      ['Jane', 'Smith', 'jane.smith@example.com'],
+      ['Bob', 'Jones', 'bob.jones@example.com'],
+    ];
 
-  //const creds = require('.././google-credentials.json');
+    const credsString = process.env["GOOGLE_CERT"]!;
+    const creds = JSON.parse(credsString)
 
-  const modulePath = '../../../../google-credentials.json';
-
-  import(modulePath)
-    .then((creds) => {
-
-      async function accessSpreadsheet() {
-        await doc.useServiceAccountAuth({
-          client_email: creds.client_email,
-          private_key: creds.private_key,
-        });
-
-        await doc.loadInfo();
-      }
-
-      async function writeToSpreadsheet() {
-        const sheet = doc.sheetsByIndex[0];
-
-        await sheet.addRows(rows);
-      }
-
-      accessSpreadsheet().then(writeToSpreadsheet);
-    })
-    .catch((error) => {
-      console.error(`Failed to load module: ${modulePath}`, error);
-    });
-
-
+    async function accessSpreadsheet() {
+      await doc.useServiceAccountAuth({
+        client_email: creds.client_email,
+        private_key: creds.private_key,
+      });
+      
+      await doc.loadInfo();
+    }
+    
+    async function writeToSpreadsheet() {
+      const sheet = doc.sheetsByIndex[0];
+      
+      await sheet.addRows(rows);
+    }
+    
+    accessSpreadsheet().then(writeToSpreadsheet);
 }
